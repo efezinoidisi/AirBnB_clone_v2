13c47bf12c717b934193f83b59ea6b4bb01e33ef
Manuel254 <immanuelkituku@gmail.com>
2023-03-21 03:03:26 -0700

    Add Review class to DBStorage and relationships to Place and User classes.This commit adds the Review class to the DBStorage and creates relationships between the Place and User classes. Additionally, a property is created in the Place class to return a list of Review instances

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -39,7 +39,7 @@ class DBStorage:
         from models.place import Place
         from models.review import Review
 
-        classes = [State, City, User, Place]
+        classes = [State, City, User, Place, Review]
         dictionary = {}
         if cls:
             objs = self.__session.query(cls)
--- models/place.py
+++ models/place.py
@@ -2,6 +2,8 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class Place(BaseModel, Base):
@@ -18,3 +20,10 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     amenity_ids = []
+    reviews = relationship("Review", backref="place", cascade="all, delete")
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def reviews(self):
+            """Returns the list of Review instances"""
+            return [review for review in reviews]
