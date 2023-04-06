56a198bf2d31677c15ea85b65b1e05d65670fdb3
efezino idisi <enduranceidisi13@gmail.com>
2023-04-06 17:14:09 +0100

    check if command is successful

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""This module contains the Fabric script do_pack which generates a .tgz archive from the contents of the web_static folder of this repository"""
+"""This module contains the Fabric script do_pack which generates a .tgz
+archive from the contents of the web_static folder of this repository"""
 
 from fabric.api import local
 from datetime import datetime
@@ -7,9 +8,11 @@ from datetime import datetime
 
 def do_pack():
     """generates a .tgz archive from the contents of the web_static folder"""
-    
     current_date = datetime.now().strftime("%Y%m%d%H%M%S")
     filename = "web_static_{}.tgz".format(current_date)
     local('mkdir -p versions')
-    local('tar -cvzf {} ./web_static'.format(filename))
-    return filename
+    result = local('tar -cvzf {} ./web_static'.format(filename))
+    if result.return_code == 0:
+        return filename
+    else:
+        return None
