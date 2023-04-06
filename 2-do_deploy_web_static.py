061471851607c33623ebad392a1166e9e4d94b73
efezino idisi <enduranceidisi13@gmail.com>
2023-04-06 21:21:28 +0100

    add script that uploads an archive, uncompress the archive and deploys it

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,19 +8,22 @@ from os import path
 
 env.use_ssh_config = True
 env.hosts = ['134559-web-02', '134559-web-01']
+
+
 def do_deploy(archive_path):
-	"""upload an archieve to server"""
-	if not path.exists(archive_path):
-		return False
-	put(archive_path, "/tmp/")
-	filename = archive_path.split('/')[1]
-	filepath = "/tmp/" + filename
-	no = filename.split('.')[0]
-	des = '/data/web_static/releases/' + no
-	run('mkdir {}'.format(des))
-	run('tar -xvzf {} -C {}/'.format(filepath, des))
-	run('rm {}'.format(filepath))
-	run('rm /data/web_static/current')
-	run('ln -s {} /data/web_static/current'.format(des)
-	)
-	return True
+    """upload an archieve to server"""
+    if not path.exists(archive_path):
+        return False
+    put(archive_path, "/tmp/")
+    filename = archive_path.split('/')[1]
+    filepath = "/tmp/" + filename
+    name = filename.split('.')[0]
+    des = '/data/web_static/releases/' + name
+    run('mkdir {}'.format(des))
+    run('tar -xvzf {} -C {}/'.format(filepath, des))
+    run('rm {}'.format(filepath))
+    run('rm -rf /data/web_static/current')
+    first = 'mv /data/web_static/releases/{}/web_static/*'.format(name)
+    run('{} /data/web_static/releases/{}/'.format(first, name, name))
+    run('ln -s {}/ /data/web_static/current'.format(des))
+    return True
