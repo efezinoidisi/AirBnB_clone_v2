f8866917215e663ba176b3d3b3465793fdbb9923
efezino idisi <enduranceidisi13@gmail.com>
2023-04-06 17:04:59 +0100

    use format string

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-"""This module contains the fabric script do_pack which generates a .tgz"""
+"""This module contains the Fabric script do_pack which generates a .tgz archive from the contents of the web_static folder of this repository"""
+
 from fabric.api import local
 from datetime import datetime
 
 
 def do_pack():
     """generates a .tgz archive from the contents of the web_static folder"""
-    cd = datetime.now()  # current date varaible
-    one = f'versions/web_static_{cd.year}{cd.month:02d}{cd.day:02d}'
-    two = f'{cd.hour:02d}{cd.minute:02d}{cd.second:02d}.tgz'
-    filename = one + two
-    print(filename)
+    
+    current_date = datetime.now().strftime("%Y%m%d%H%M%S")
+    filename = "web_static_{}.tgz".format(current_date)
     local('mkdir -p versions')
-    local(f'tar -cvzf {filename} ./web_static')
+    local('tar -cvzf {} ./web_static'.format(filename))
     return filename
