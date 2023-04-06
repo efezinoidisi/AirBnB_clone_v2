bbd75d7336b4e49848fd13c615f05e9b4fb84593
Efezino Idisi <enduranceidisi13@gmail.com>
2023-04-06 18:17:04 +0000

    add script that pushes archive to server

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,9 +7,20 @@ from os import path
 
 
 env.use_ssh_config = True
-env.hosts = ['one']
+env.hosts = ['134559-web-02', '134559-web-01']
 def do_deploy(archive_path):
-    """generates a .tgz archive from the contents of the web_static folder"""
-    if not path.exists(archive_path):
-        return False
-    put(archive_path, "/tmp/")
+	"""upload an archieve to server"""
+	if not path.exists(archive_path):
+		return False
+	put(archive_path, "/tmp/")
+	filename = archive_path.split('/')[1]
+	filepath = "/tmp/" + filename
+	no = filename.split('.')[0]
+	des = '/data/web_static/releases/' + no
+	run('mkdir {}'.format(des))
+	run('tar -xvzf {} -C {}/'.format(filepath, des))
+	run('rm {}'.format(filepath))
+	run('rm /data/web_static/current')
+	run('ln -s {} /data/web_static/current'.format(des)
+	)
+	return True
