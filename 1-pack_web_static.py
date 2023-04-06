64b960377b3d1e744327ef143079c2b53998cb6b
efezino idisi <enduranceidisi13@gmail.com>
2023-04-06 16:32:54 +0100

    improve module comment

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,8 +7,7 @@ from datetime import datetime
 
 
 def do_pack():
-    """
-    generates a .tgz archive from the contents of the web_static folder
+    """generates a .tgz archive from the contents of the web_static folder
     """
     cd = datetime.now()  # current date varaible
     one = f'versions/web_static_{cd.year}{cd.month:02d}{cd.day:02d}'
