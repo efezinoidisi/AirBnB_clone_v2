4e97e10e144ac63beb5eefc1c20e44259dff0ed1
efezino idisi <enduranceidisi13@gmail.com>
2023-04-20 21:14:12 +0100

    add new route 'hbnb' and remove new line

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This module contains a single route '/'
+This module contains a two routes '/' and 'hello'
 """
 from flask import Flask
 
@@ -12,6 +12,10 @@ app = Flask(__name__)
 def hello_hbnb():
     """This route returns hello hbnb"""
     return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
 
 
 if __name__ == "__main__":
