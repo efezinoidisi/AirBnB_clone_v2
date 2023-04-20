347e3d0ddc5bdcd3f7c4eebee5026d92fa2aef69
efezino idisi <enduranceidisi13@gmail.com>
2023-04-20 21:07:21 +0100

    add comments and new line

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """
+This module contains a single route '/'
 """
 from flask import Flask
 
@@ -8,7 +9,8 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    return 'Hello HBNB!'
+    """This route returns hello hbnb"""
+    return 'Hello HBNB!\n'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
