e5089f242ad8f90adf935d57b0edf354944bffd6
efezino idisi <enduranceidisi13@gmail.com>
2023-04-22 23:32:45 +0100

    add new route cities_by_states

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module contains a simple flask application
+"""
+from flask import Flask, render_template
+from werkzeug.utils import escape
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_state():
+    data = storage.all(State).values()
+    return render_template('7-states_list.html', data=data)
+
+
+@app.teardown_appcontext
+def close_db(error):
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
