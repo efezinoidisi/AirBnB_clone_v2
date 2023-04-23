13cb3a5e8977f8ac04e6eb455de0328b75f66eeb
efezino idisi <enduranceidisi13@gmail.com>
2023-04-23 22:58:58 +0100

    complete advanced task

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -6,16 +6,17 @@ from flask import Flask, render_template
 from models import storage
 from models.state import State
 from models.amenity import Amenity
-
+#from models.user import User
+from models.place import Place
 app = Flask(__name__)
 
 
-@app.route('/hbnb_filters', strict_slashes=False)
-def filters():
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
     states = storage.all(State).values()
     amenities = storage.all(Amenity).values()
-    return render_template('10-hbnb_filters.html', states=states,
-                           amenities=amenities)
+    places = storage.all(Place).values()
+    return render_template('100-hbnb.html', states=states, amenities=amenities, places=places)
 
 
 @app.teardown_appcontext
